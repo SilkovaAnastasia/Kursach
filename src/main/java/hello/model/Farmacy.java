139d1fc559a5353c6b31fc114f68a8b1ea676197
SilkovaAnastasia <trapanastasia@gmail.com>
2017-05-23 11:56:13 +0300

    model

--- src/main/java/hello/model/Farmacy.java
+++ src/main/java/hello/model/Farmacy.java
@@ -1,22 +1,105 @@
 package hello.model;
+import sun.awt.image.SunWritableRaster;
+import sun.util.calendar.LocalGregorianCalendar;
 
 import javax.persistence.*;
 
-/**
- * Created by Анастасия on 13.05.2017.
- */
 @Entity
-@Table(name="Farmacy")
+@Table(name = "FarmacyAll")
 public class Farmacy {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-        private Integer id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer n_farmacy;
 
-        @Column(name="type")
-        private String FarmName;
-        private String FDir;
-        private String IDir;
-        private String ODir;
-        private String Address;
+    @Column(name = "farmacy")
+    private String fName;
+    private String dSurname;
+    private String dName;
+    private String dMiddleName;
+    private int fBuild;
+    private String fStreet;
+    private String fCity;
+    private String fCountry;
+    private int fIndex;
+
+    public int getN_farmacy() {
+        return n_farmacy;
+    }
+
+    public void setN_farmacy(int n_farmacy) {
+        this.n_person = n_farmacy;
+    }
+
+    public String getFName() {
+        return fName;
+    }
+
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getDSurname() {
+        return dSurname;
+    }
+
+    public void setDSurname(String dSurname) {
+        this.dSurname = dSurname;
+    }
+
+    public String getDName() {
+        return dName;
+    }
+
+    public void setDName(String dName) {
+        this.dName = dName;
+    }
+
+    public String getDMiddleName() {
+        return dMiddleName;
+    }
+
+    public void setDMiddleName(String dMiddleName) {
+        this.surname = dMiddleName;
+    }
+
+    public int getFBuild() {
+        return fBuild;
+    }
+
+    public void setFBuild(int fBuild) {
+        this.fBuild = fBuild;
+    }
+
+    public String getFStreet() {
+        return fStreet;
+    }
+
+    public void setFStreet(String fStreet) {
+        this.fStreet = fStreet;
+    }
+
+    public String getFCity() {
+        return fCity;
+    }
+
+    public void setFCity(String fCity) {
+        this.fCity = fCity;
+    }
+
+    public String getFCountry() {
+        return fCountry;
+    }
+
+    public void setFCountry(String fCountry) {
+        this.fCountry = fCountry;
+    }
+
+    public int getFIndex() {
+        return fIndex;
+    }
+
+    public void setFIndex(int fIndex) {
+        this.fIndex = fIndex;
+    }
 }
--- src/main/java/hello/model/Producer.java
+++ src/main/java/hello/model/Producer.java
@@ -1,25 +1,133 @@
-package hello.model;
+﻿package hello.model;
+import sun.awt.image.SunWritableRaster;
+import sun.util.calendar.LocalGregorianCalendar;
 
 import javax.persistence.*;
 
-/**
- * Created by Анастасия on 13.05.2017.
- */
+
 @Entity
-@Table(name="Producer")
+@Table(name="ProducerAll")
 public class Producer {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    private Integer id;
+    private Integer n_producer;
 
-    @Column(name="type")
-    private String ProdName;
-    private String FDir;
-    private String IDir;
-    private String ODir;
-    private String Address;
-    private String MedName;
-    private int SrokGodn;
-    private int DateIzg;
+    @Column(name="Producer")
+    private String pName;
+    private String pdSurname;
+    private String pdName;
+    private String pdMiddleName;
+    private int pBuild;
+    private String pStreet;
+    private String pCity;
+    private String pCountry;
+    private int pIndex;
+    private String medName;
+    private int expDate;
+    private int dateOfProduction;
+
+    public int getN_producer() {
+        return n_producer;
+    }
+
+    public void setN_producer(int n_producer) {
+        this.n_producer = n_producer;
+    }
+
+    public String getPName() {
+        return pName;
+    }
+
+    public void setPName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getPDSurname() {
+        return pdSurname;
+    }
+
+    public void setPDSurname(String pdSurname) {
+        this.pdSurname = pdSurname;
+    }
+
+    public String getPDName() {
+        return pdName;
+    }
+
+    public void setPDName(String PDName) {
+        this.pdName = pdName;
+    }
+
+    public String getPDMiddleName() {
+        return pdMiddleName;
+    }
+
+    public void setPDMiddleName(String pdMiddleName) {
+        this.pdSurname = pdMiddleName;
+    }
+
+    public int getPBuild() {
+        return pBuild;
+    }
+
+    public void setPBuild(int pBuild) {
+        this.pBuild = pBuild;
+    }
+
+    public String getPStreet() {
+        return pStreet;
+    }
+
+    public void setPStreet(String pStreet) {
+        this.pStreet = pStreet;
+    }
+
+    public String getPCity() {
+        return pCity;
+    }
+
+    public void setPCity(String pCity) {
+        this.pCity = pCity;
+    }
+
+    public String getPCountry() {
+        return pCountry;
+    }
+
+    public void setPCountry(String pCountry) {
+        this.pCountry = pCountry;
+    }
+
+    public int getPIndex() {
+        return pIndex;
+    }
+
+    public void setPIndex(int pIndex) {
+        this.pIndex = pIndex;
+    }
+
+    public String getMedName() {
+        return medName;
+    }
+
+    public void setMedName(String medName) {
+        this.medName = medName;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public String getDateOfProduction() {
+        return dateOfProduction;
+    }
+
+    public void setDateOfProduction(String dateOfProduction) {
+        this.dateOfProduction = dateOfProduction;
+    }
 }
